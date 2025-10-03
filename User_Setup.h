9106ea4c551ce853ce644a5272245a54a1d371b2
hhhh475017355-cloud <hhhh475017355@gmail.com>
2025-10-04 00:41:00 +0800

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -166,15 +166,13 @@
 
 // ###### EDIT THE PIN NUMBERS IN THE LINES FOLLOWING TO SUIT YOUR ESP8266 SETUP ######
 
-// For NodeMCU - use pin numbers in the form PIN_Dx where Dx is the NodeMCU pin designation
-#define TFT_MISO  PIN_D6  // Automatically assigned with ESP8266 if not defined
-#define TFT_MOSI  PIN_D7  // Automatically assigned with ESP8266 if not defined
-#define TFT_SCLK  PIN_D5  // Automatically assigned with ESP8266 if not defined
-
-#define TFT_CS    PIN_D8  // Chip select control pin D8
-#define TFT_DC    PIN_D3  // Data Command control pin
-#define TFT_RST   PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
-//#define TFT_RST  -1     // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
+#define TFT_MISO 19
+#define TFT_MOSI 23    // LCD_HR
+#define TFT_SCLK 18    // LCD_RD
+#define TFT_CS    5    // LCD_CS - Chip select control pin
+#define TFT_DC   19    // LCD_RS - Data Command control pin
+#define TFT_RST  21    // LCD_RST - Reset pin
+//#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 
 //#define TFT_BL PIN_D1  // LED back-light (only for ST7789 with backlight control pin)
